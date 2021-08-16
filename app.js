9c36fc640ff9b9096984a72fcf55b70c0454f2b2
xenogenesis20 <xenogenesis20@outlook.com>
2021-08-16 01:32:12 +0000

    having validation errors

--- app.js
+++ app.js
@@ -3,10 +3,17 @@ const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
 const connectDB = require('./config/db')
+const passport = require('passport')
+const session = require('express-session')
 const path = require('path')
 // load config
-dotenv.config({path: '.config/config.env'})
+dotenv.config({path: './config/config.env'})
+
 console.log("mongo uri: ",process.env.MONGO_URI)
+
+//passport config
+require('./config/passport')(passport)
+
 connectDB();
 
 const app = express()
@@ -19,11 +26,25 @@ if (process.env.NODE_ENV === 'development'){
 app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
+//session middleware
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    
+  }))
+
+//passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
 //static folder
 app.use(express.static(path.join(__dirname,'public')));
 
 //routes
 app.use('/',require('./routes/index'))
+app.use('/auth',require('./routes/auth'))
+
 
 const PORT = process.env.PORT || 3000
 
---
+++ routes/auth.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const router = express.Router()
+const passport = require('passport')
+
+//@desc auth with google
+//@route GET /auth/google
+router.get('/google', passport.authenticate('google',{ scope: ['profile'] }))
+
+//@desc google auth callback
+//@route GET /auth/google/callback
+router.get('/google/callback',passport.authenticate('google', {failureRedirect: '/' }),
+(req, res)=>{
+    res.redirect('/dashboard')
+})
+
+//@desc logout user
+//@route /auth/logout
+router.get('/logout', (req, res) => {
+    req.logout()
+    res.redirect('/')
+})
+
+
+module.exports = router
